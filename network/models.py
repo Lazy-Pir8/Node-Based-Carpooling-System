49c27c3fdb84163a9ab2c666a3f8636eecae8086
lazy-pir8 <harshsahuxlr9@gmail.com>
2026-02-27 02:51:40 +0530

    Little Progress

--- network/models.py
+++ network/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Node(models.Model):
+    point = models.CharField(max_length=100)
+
+class Edge(models.Model):
+    from_node = models.ForeignKey(Node, related_name = "from_node", on_delete=models.CASCADE)
+    to_node = models.ForeignKey(Node, related_name = "to_node", on_delete=models.CASCADE)
